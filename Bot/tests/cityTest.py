8ca2760c0ae6645b4c81e2173293d1d6130f3a9f
EvilOny <110058595+EvilOny@users.noreply.github.com>
2023-12-22 01:08:31 +0300

    Add files via upload

---
+++ Bot/tests/cityTest.py
@@ -0,0 +1,18 @@
+import unittest
+from Bot.data.tools import city
+
+
+class CityTest(unittest.TestCase):
+    def test_Severodvinsk(self):
+        lat, lon = city('Северодвинск')
+        self.assertEqual(lat, 64.563385)
+        self.assertEqual(lon, 39.823769)
+
+    def test_Arkhangelsk(self):
+        lat, lon = city('Arkhangelsk')
+        self.assertEqual(lat, 64.543022)
+        self.assertEqual(lon, 40.537121)
+
+
+if __name__ == '__main__':
+    unittest.main()
