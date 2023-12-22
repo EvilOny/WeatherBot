36da7833a2be88660938ecfd1de5447a7d2b35df
EvilOny <110058595+EvilOny@users.noreply.github.com>
2023-12-22 11:13:12 +0300

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from src.tools.tools import createBot
+
+Token = 'ваш токен'
+
+
+def main():
+    createBot(Token)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
---
+++ src/tools/config.py
@@ -0,0 +1,4 @@
+WEATHER_URL = ''
+FORECAST_URL = ''
+GEO_URL = ''
+API_KEY = ''
